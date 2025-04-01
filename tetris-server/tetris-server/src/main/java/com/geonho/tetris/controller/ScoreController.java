3f3ddefa094d1827c83bb9d6627e507b80def926
이건호 <dlrjsgh367@gmail.com>
2025-04-01 14:47:09 +0900

    Feat: Score Controller

---
+++ tetris-server/tetris-server/src/main/java/com/geonho/tetris/controller/ScoreController.java
@@ -0,0 +1,28 @@
+package com.geonho.tetris.controller;
+
+import com.geonho.tetris.dto.ScoreRequest;
+import com.geonho.tetris.dto.ScoreResponse;
+import com.geonho.tetris.service.ScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ScoreController {
+    private final ScoreService scoreService;
+
+    @PostMapping("/score")
+    public ResponseEntity<?> submitScore(@RequestBody ScoreRequest req) {
+        scoreService.saveScore(req);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/rank")
+    public ResponseEntity<List<ScoreResponse>> getTopScores() {
+        return ResponseEntity.ok(scoreService.getTop10Scores());
+    }
+}
--- tetris-server/tetris-server/src/test/java/com/geonho/tetris/TetrisServerApplicationTests.java
+++ tetris-server/tetris-server/src/test/java/com/geonho/tetris/TetrisServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.geonho.tetris.tetris_server;
+package com.geonho.tetris;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
