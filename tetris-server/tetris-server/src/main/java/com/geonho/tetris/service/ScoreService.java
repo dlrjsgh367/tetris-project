f9d51cd82c1745bfa2677627d4de2ef7f62fbb0d
이건호 <dlrjsgh367@gmail.com>
2025-04-01 14:46:27 +0900

    Feat: Score Service

---
+++ tetris-server/tetris-server/src/main/java/com/geonho/tetris/service/ScoreService.java
@@ -0,0 +1,32 @@
+package com.geonho.tetris.service;
+
+import com.geonho.tetris.dto.ScoreRequest;
+import com.geonho.tetris.dto.ScoreResponse;
+import com.geonho.tetris.entity.Score;
+import com.geonho.tetris.repository.ScoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScoreService {
+
+    private final ScoreRepository scoreRepository;
+
+    public void saveScore(ScoreRequest req) {
+        Score score = Score.builder()
+                .player(req.getPlayer())
+                .score(req.getScore())
+                .duration(req.getDuration())
+                .createdAt(LocalDateTime.now())
+                .build();
+        scoreRepository.save(score);
+    }
+
+    public List<ScoreResponse> getTop10Scores() {
+        return scoreRepository.findTop10ByOrderByScoreDesc();
+    }
+}
